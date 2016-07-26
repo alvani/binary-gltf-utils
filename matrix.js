4fec81b1c84a56fc73a1601d78c65d80cad2aa83
Alvani Wiwoho <alpha2_yui@yahoo.com>
2016-07-26 15:00:01 +0700

    implement create translation matrix, create unity local rotation matrix ex, and add column param on create rotation matrix

--- matrix.js
+++ matrix.js
@@ -10,7 +10,7 @@ function toRadians(degree) {
 }
 
 // (yaw, pitch, roll) = (z axis, x axis, y axis)
-exports.createRotationMatrix = function(yaw, pitch, roll, cw) {
+exports.createRotationMatrix = function(yaw, pitch, roll, cw, column) {
 	var mul = cw ? -1 : 1;
 	var a = toRadians(yaw);
 	var cosa = Math.cos(a);
@@ -23,7 +23,7 @@ exports.createRotationMatrix = function(yaw, pitch, roll, cw) {
 		0, 0, 1, 0,
 		0, 0, 0, 1
 	];
-	var m = this.arrToMatrix(myaw);					 
+	var m = this.arrToMatrix(myaw, column);			 
 	
 	a = toRadians(pitch);
 	cosa = Math.cos(a);
@@ -36,7 +36,7 @@ exports.createRotationMatrix = function(yaw, pitch, roll, cw) {
 		0, sina, cosa, 0,
 		0, 0, 0, 1
 	];
-	var m2 = this.arrToMatrix(mpitch);
+	var m2 = this.arrToMatrix(mpitch, column);
 	m = math.multiply(m, m2);
 
 	a = toRadians(roll);
@@ -50,7 +50,7 @@ exports.createRotationMatrix = function(yaw, pitch, roll, cw) {
 		sina, 0, cosa, 0,
 		0, 0, 0, 1
 	];
-	m2 = this.arrToMatrix(mroll);
+	m2 = this.arrToMatrix(mroll, column);
 	m = math.multiply(m, m2);
 	return m;
 };
@@ -58,6 +58,29 @@ exports.createRotationMatrix = function(yaw, pitch, roll, cw) {
 exports.createUnityRotationMatrix = function(yaw, pitch, roll) {
 	return this.createRotationMatrix(roll, pitch, yaw, true);
 };
+
+exports.createUnityLocalRotationMatrixEx = function(offsetX, offsetY, offsetZ, yaw, pitch, roll, scaleX, scaleY, scaleZ) {
+	
+	//var trans = new Cesium.Cartesian3(44.2, -131.2, -96.2);
+        //var transMatrix = Cesium.Matrix4.fromTranslation(trans);
+
+		
+
+	var uniMat = this.createRotationMatrix(0, -90, 0, true); // unity will transform -90 cw x axis from blender file		
+	var posMat = this.createRotationMatrix(yaw, pitch, roll, true);
+	var invUniMat = math.inv(uniMat);
+	var rotMat = math.multiply(posMat, invUniMat);	
+
+	// var rotMat = this.createRotationMatrix(-149.2203, 0, 0, true);	
+
+	// should it be rotated 180 to match cesium coord ??
+	var adjMat = this.createRotationMatrix(180, 0, 0, true);
+
+	var offMat = this.createTranslationMatrix(44.2, -131.2, -96.2);		
+	var res = math.multiply(offMat, math.multiply(rotMat, adjMat));	
+
+	return res;
+}
 
 // blendMat: row major blender rotation matrix
 exports.createUnityLocalRotationMatrix = function(blendMat, yaw, pitch, roll, scaleX, scaleY, scaleZ) {
@@ -80,6 +103,16 @@ exports.createUnityLocalRotationMatrix = function(blendMat, yaw, pitch, roll, sc
 	// console.log(localMat);
 	return localMat;
 };
+
+exports.createTranslationMatrix = function(x, y, z) {
+	var arr = [
+		1, 0, 0, x,
+		0, 1, 0, y,
+		0, 0, 1, z,
+		0, 0, 0, 1
+	];
+	return this.arrToMatrix(arr);
+}
 
 exports.createScaleMatrix = function(x, y, z) {
 	var arr = [
